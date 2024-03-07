d5311ed56ca8602b05a12d0ac34704c85e3762e5
¨miguelamador23¨ <miguelamador23@outlook.com>
2024-03-07 11:15:10 -0600

    migraciones

---
+++ Backend/app/Http/Controllers/EstadoIncidenciaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Estado_incidencia;
+use Illuminate\Http\Request;
+
+class EstadoIncidenciaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Estado_incidencia $estado_incidencia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Estado_incidencia $estado_incidencia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Estado_incidencia $estado_incidencia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Estado_incidencia $estado_incidencia)
+    {
+        //
+    }
+}
